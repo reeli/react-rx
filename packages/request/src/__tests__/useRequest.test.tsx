0b5a60895f2be6a1db23818701042d3b0b568bc1
reeli <reeli.null@gmail.com>
2022-12-01 17:09:40 +0800

    add tests

--- packages/request/src/__tests__/useRequest.test.tsx
+++ packages/request/src/__tests__/useRequest.test.tsx
@@ -1,40 +1,59 @@
-import { renderHook } from "@testing-library/react-hooks";
+import { renderHook } from "@testing-library/react";
 import axios from "axios";
-import type {
-  ReactNode,
-} from "react";
+import nock from "nock";
+import type { ReactNode } from "react";
 import { createRequest, RequestProvider } from "../";
 import { useRequest } from "../useRequest";
 
-const wrapper = ({ children }: { children: ReactNode }) => (
-  <RequestProvider value={{ axiosInstance: axios.create() }}>{children}</RequestProvider>
-);
-
 describe("useRequest", () => {
-  it("should return request function and requestStage$", () => {
-    const getTestDataUsingGet = createRequest<
-      { id?: string },
-      {
-        name: string;
-        age: number;
-      }
-    >("getTestDataUsingGet", ({ id }) => ({
-      url: `/api/tests/${id}`,
-      method: "get",
-    }));
+  const basePath = "http://localhost:8080";
+  const wrapper = ({ children }: { children: ReactNode }) => (
+    <RequestProvider value={{ axiosInstance: axios.create({ baseURL: "http://localhost:8080" }) }}>
+      {children}
+    </RequestProvider>
+  );
+  const getTestDataUsingGet = createRequest<
+    { id?: string },
+    {
+      name: string;
+      age: number;
+    }
+  >("getTestDataUsingGet", ({ id }) => ({
+    url: `/api/tests/${id}`,
+    method: "get",
+  }));
+
+  it("should notify response data and status when request succeed", (done) => {
+    nock(basePath).get("/api/tests/001").reply(200, { name: "Tom", age: 6 }, { "Access-Control-Allow-Origin": "*" });
+
     const { result } = renderHook(() => useRequest(getTestDataUsingGet), { wrapper });
-    const [getTestData] = result.current;
+    const [getTestData, request$] = result.current;
 
     getTestData({ id: "001" });
 
-    // expect(getTestDataUsingGet).toHaveBeenCalledWith(
-    //   expect.objectContaining({
-    //     type: "getTestDataUsingGet",
-    //     payload: {
-    //       method: "get",
-    //       url: "/api/tests/001",
-    //     },
-    //   }),
-    // );
+    request$?.subscribe(({ status, data, error }) => {
+      expect(data).toEqual({ name: "Tom", age: 6 });
+      expect(status).toEqual("success");
+      expect(error?.message).toEqual(undefined);
+      done();
+    });
+  });
+
+  it("should notify error and status when request fail", (done) => {
+    nock(basePath)
+      .get("/api/tests/001")
+      .reply(500, { message: "Something went wrong" }, { "Access-Control-Allow-Origin": "*" });
+
+    const { result } = renderHook(() => useRequest(getTestDataUsingGet), { wrapper });
+    const [getTestData, data$] = result.current;
+
+    getTestData({ id: "001" });
+
+    data$?.subscribe(({ status, data, error }) => {
+      expect(status).toEqual("failure");
+      expect(error?.response?.data).toEqual({ message: "Something went wrong" });
+      expect(data).toEqual(undefined);
+      done();
+    });
   });
 });
--- packages/request/src/useRequest.ts
+++ packages/request/src/useRequest.ts
@@ -1,21 +1,48 @@
+import { AxiosError } from "axios";
 import { useContext, useMemo, useRef } from "react";
 import { Subject } from "rxjs";
 import { CreateRequestReturn } from "./createRequest";
 import { RequestContext } from "./RequestContext";
 
+enum RequestStatus {
+  pending = "pending",
+  success = "success",
+  failure = "failure",
+}
+
+interface RequestResult<TResp> {
+  status: RequestStatus;
+  data?: TResp;
+  error?: AxiosError;
+}
+
 export const useRequest = <TReq, TResp>(fn: CreateRequestReturn<TReq, TResp>) => {
-  const dataRef = useRef<Subject<TResp>>();
+  const dataRef = useRef<Subject<RequestResult<TResp>>>();
   const { axiosInstance } = useContext(RequestContext);
 
   const requestFn = useMemo(() => {
-    const requestObserver$ = new Subject<TResp>();
-    dataRef.current = requestObserver$;
+    const request$ = new Subject<RequestResult<TResp>>();
+    dataRef.current = request$;
 
     return (params: TReq) => {
-      return axiosInstance.request(fn(params)).then((resp) => {
-        requestObserver$.next(resp.data);
-        return resp;
-      });
+      request$.next({ status: RequestStatus.pending });
+
+      return axiosInstance
+        .request(fn(params))
+
+        .then((resp) => {
+          request$.next({
+            status: RequestStatus.success,
+            data: resp.data,
+          });
+          return resp;
+        })
+        .catch((error) => {
+          request$.next({
+            status: RequestStatus.failure,
+            error,
+          });
+        });
     };
   }, []);
 
